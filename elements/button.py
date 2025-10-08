2f27e702b0933752065147a85c1d6a38c254c5a0
PlohoSpaal <bulzigan.bz@gmail.com>
2025-10-08 19:23:56 +0300

    Added Textarea coverage

--- elements/button.py
+++ elements/button.py
@@ -2,7 +2,7 @@ from playwright.sync_api import expect
 from elements.base_element import BaseElement
 import allure
 from tools.logger import get_logger
-
+from ui_coverage_tool import ActionType
 
 logger = get_logger('BUTTON')
 
@@ -19,9 +19,13 @@ class Button(BaseElement):
             locator = self.get_locator(nth, **kwargs)
             expect(locator).to_be_enabled()
 
+        self.track_coverage(ActionType.ENABLED, nth, **kwargs)
+
     def check_disabled(self, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" is disabled'
         with allure.step(step):
             logger.info(step)
             locator = self.get_locator(nth, **kwargs)
             expect(locator).to_be_disabled()
+
+        self.track_coverage(ActionType.DISABLED, nth, **kwargs)
--- elements/input.py
+++ elements/input.py
@@ -2,6 +2,7 @@ from playwright.sync_api import expect, Locator
 from elements.base_element import BaseElement
 import allure
 from tools.logger import get_logger
+from ui_coverage_tool import ActionType
 
 
 logger = get_logger('INPUT')
@@ -15,12 +16,17 @@ class Input(BaseElement):
     def get_locator(self, nth: int = 0, **kwargs) -> Locator:
         return super().get_locator(nth, **kwargs).locator('input')
 
+    def get_raw_locator(self, nth: int = 0, **kwargs) -> str:
+        return f'{super().get_raw_locator(**kwargs)}//input'
+
     def fill(self, value: str, nth: int = 0, **kwargs):
         step = f'Fill {self.type_of} "{self.name}" to value "{value}"'
         with allure.step(step):
             logger.info(step)
             locator = self.get_locator(nth, **kwargs)
             locator.fill(value)
+
+        self.track_coverage(ActionType.FILL, nth, **kwargs)
 
     def clean(self, nth: int = 0, **kwargs):
         step = f'Clear {self.type_of} "{self.name}" from value'
@@ -35,3 +41,5 @@ class Input(BaseElement):
             logger.info(step)
             locator = self.get_locator(nth, **kwargs)
             expect(locator).to_have_value(value)
+
+        self.track_coverage(ActionType.VALUE, nth, **kwargs)
